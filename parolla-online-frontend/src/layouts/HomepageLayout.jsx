9493c694afb90a85b96b76a17b67aa5122baf179
gorkemozbay <gorkemozbay0405@gmail.com>
2025-03-20 23:53:22 +0300

    HomePage is refactored.

--- parolla-online-frontend/src/layouts/HomepageLayout.jsx
+++ parolla-online-frontend/src/layouts/HomepageLayout.jsx
@@ -1,8 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Stack, IconButton, Select, MenuItem} from '@mui/material';
-import SettingsIcon from '@mui/icons-material/Settings';
 import languageENG from '../local/languageENG';
 import languageTR from '../local/languageTR';
 import { useDispatch, useSelector } from "react-redux";
@@ -10,7 +7,6 @@ import { setLanguage } from "../redux/languageSlice";
 
 const HomepageLayout = () => {
 
-    const theme = useTheme();
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
@@ -23,52 +19,103 @@ const HomepageLayout = () => {
 
     return (
         <>
-            <Box position="absolute" top={16} left={16}>
-                <IconButton>
-                    <SettingsIcon />
-                </IconButton>
-            </Box>
-            <Box position="absolute" top={16} right={16}>
-                <Select
+            <div 
+                style={{
+                    position: "absolute",
+                    top: "16px",
+                    right: "32px"
+                }}
+            >
+                <select
+                    style={{ 
+                        backgroundColor: "white",
+                        color: "black",
+                        height: "35px",
+                        width: "70px",
+                        padding: "5px",
+                        borderRadius: "5px",
+                        border: "1px black solid",
+                        fontFamily: "Chevy, cursive",
+                    }}
                     value={languageChoice}
                     onChange={(e) => dispatch(setLanguage(e.target.value))}
-                    displayEmpty
-                    variant="outlined"
-                    sx={{ height: 35, maxWidth: 100, backgroundColor: "white" }}
                 >
-                    <MenuItem value="ENG">ENG</MenuItem>
-                    <MenuItem value="TR">TR</MenuItem>
-                </Select>
-            </Box>
-            <Box
-                component="img"
-                src="images/logo_ai2_transparent.png"
-                alt="Logo"
-                sx={{ width: "auto", height: "320px", marginBottom: "100px", display: "block", marginLeft: "auto", marginRight: "auto" }}
+                    <option  value="ENG">ENG</option >
+                    <option  value="TR">TR</option >
+                </select>
+            </div>
+            <div 
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
             >
-            </Box>
-            <Stack spacing={1} direction="column" alignItems="center" justifyContent="center">
-                <Button
-                    variant="contained"
-                    sx={{ width: 600, backgroundColor: theme.palette.colors.blue }}
+                <img
+                    style={{ 
+                        height: "400px", 
+                        marginBottom: "50px", 
+                    }}
+                    src="images/logo_ai2_transparent.png"
+                    alt="Logo"
+                >
+                </img>
+                <button
+                    style={{
+                        backgroundColor: "#FFD65A" ,
+                        height: "50px",
+                        width: "600px",
+                        border: "2px solid black",
+                        borderRadius: "10px",
+                        color: "black",
+                        cursor: "pointer",
+                        fontSize: "1.2em",
+                        fontWeight: "bold",
+                        margin: "10px",
+                        fontFamily: "Chevy, cursive",
+                    }}
                     onClick={() => { navigate('/create-lobby') }}
                 >
                     {language.homepage.createLobbyButton}
-                </Button>
-                <Button
-                    variant="contained"
-                    sx={{ width: 600, backgroundColor: theme.palette.colors.purple }}
+                </button>
+                <button
+                    style={{
+                        backgroundColor: "#FFD65A" ,
+                        height: "50px",
+                        width: "600px",
+                        border: "2px solid black",
+                        borderRadius: "10px",
+                        color: "black",
+                        cursor: "pointer",
+                        fontSize: "1.2em",
+                        fontWeight: "bold",
+                        margin: "10px",
+                        fontFamily: "Chevy, cursive",
+                    }}
+                    onClick={() => {}}
                 >
                     {language.homepage.joinLobbyButton}
-                </Button>
-                <Button
-                    variant="contained"
-                    sx={{ width: 600, backgroundColor: theme.palette.colors.green }}
+                </button>
+                <button
+                    style={{
+                        backgroundColor: "#FFD65A" ,
+                        height: "50px",
+                        width: "600px",
+                        border: "2px solid black",
+                        borderRadius: "10px",
+                        color: "black",
+                        cursor: "pointer",
+                        fontSize: "1.2em",
+                        fontWeight: "bold",
+                        margin: "10px",
+                        fontFamily: "Chevy, cursive",
+                    }}
                     onClick={() => { navigate('/quick-play') }}
                 >
                     {language.homepage.quickplay}
-                </Button>
-            </Stack>
+                </button>
+            </div>
         </>
     );
 };
